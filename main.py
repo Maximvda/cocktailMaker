192d32ddd5b6cbede5b85eadf5903df108708ce3
siemie <maximvda123@gmail.com>
2023-02-05 17:25:34 +0100

    Updated txt and instruction and added mixing tab

--- main.py
+++ main.py
@@ -7,21 +7,23 @@ from kivy.lang import Builder
 from kivy.resources import resource_add_path
 from kivy.clock import mainthread
 from presenters.selector import Selector
+from presenters.mixer import Mixer
 
 class Main(MDApp):
     def build(self):
         self.title = f"Cocktail maker"
-        self.theme_cls.colors['Lime']['500'] = "#90e0ef"
-        self.theme_cls.colors['Lime']['200'] = "#ffafcc"
-        self.theme_cls.colors['Amber']['500'] = "#a2d2ff"
-        self.theme_cls.colors['Amber']['200'] = "#cdb4db"
-        self.theme_cls.primary_palette = "Lime"
-        self.theme_cls.accent_palette = "Amber"
+        #self.theme_cls.colors['Lime']['500'] = "#90e0ef"
+        #self.theme_cls.colors['Lime']['200'] = "#ffafcc"
+        self.theme_cls.colors['Lime']['500'] = "#a2d2ff"
+        self.theme_cls.colors['Lime']['200'] = "#cdb4db"
+        self.theme_cls.primary_palette = "Amber"
+        self.theme_cls.accent_palette = "Lime"
         self.root = Builder.load_file(
                 os.path.join(os.path.join(sys._MEIPASS, 'views'),'main.kv'))
 
     def on_start(self):
         self.root.ids.selector.init()
+        self.root.ids.mixer.init()
 
 if __name__ == "__main__":
     if hasattr(sys, '_MEIPASS'):
---
+++ presenters/mixer.py
@@ -0,0 +1,35 @@
+from presenters.presenter import Presenter
+
+class Mixer(Presenter):
+    def init(self):
+        self._total = 0
+        self.values = [0]*5
+        self._myIds = {
+            0: 'amaretto',
+            1: 'brandy',
+            2: 'rum',
+            3: 'triple',
+            4: 'lime'
+        }
+        self.ids.amaretto.name = 0
+        self.ids.brandy.name = 1
+        self.ids.rum.name = 2
+        self.ids.triple.name = 3
+        self.ids.lime.name = 4
+
+    def update(self, value, id):
+        self.values[id] = value
+        # Rescale other values
+        if sum(self.values) > 100:
+            toReduce = sum(self.values) - 100
+            self.values[id] -= toReduce
+            self.ids[self._myIds[id]].value = self.values[id]
+        self.total = sum(self.values)
+
+    @property
+    def total(self):
+        return self._total
+    @total.setter
+    def total(self, value):
+        self.ids.drinkBar.value = value
+        self._total = value
